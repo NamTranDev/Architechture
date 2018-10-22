581dba2b6168a9bb3efd2d91937a6c24eb053834
Nam Tran <namtran09061992@gmail.com>
2018-10-22 13:57:54 +0700

    - Remove attrs widget

--- util/src/main/java/tran/nam/util/Constant.kt
+++ util/src/main/java/tran/nam/util/Constant.kt
@@ -1,34 +1,38 @@
 package tran.nam.util
 
-import android.util.SparseArray
-
 interface Constant {
 
     interface REQUEST_CODE {
         companion object {
-            val TARGET_FRAGMENT = 123
+
         }
     }
 
     interface KEY_INTENT {
         companion object {
-            val ID_SEASON = "id season"
+
         }
     }
 
     interface KEY_ARGUMENT {
         companion object {
-            val LIST_TEAM_DATA = "List Team Data"
-            val TEAM_DATA = "Team Data"
+
         }
     }
 
-    interface KEY_INTENT_RESULT
+    interface KEY_INTENT_RESULT {
+        companion object {
 
-    interface KEY_DIALOG
+        }
+    }
+
+    interface KEY_DIALOG {
+        companion object {
+
+        }
+    }
 
     companion object {
-
         val API_URL = "https://api.football-data.org"
         val EMPTY = ""
     }
